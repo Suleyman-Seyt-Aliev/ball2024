7cb3f80784b59eaff6a68e6d8b8cff8c3e564acb
unknown <suleyman15.05.2005@gmail.com>
2025-01-18 12:22:22 +0300

    ’Пункт_4_2’

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* Страница баскетбольного мяча*/
+router.get('/basketball', function(req, res, next) {
+  res.send("<h1>Страница баскетбольного мяча</h1>")
+  });
+  /* Страница футбольного мяча*/
+router.get('/football', function(req, res, next) {
+  res.send("<h1>Страница футбольного мяча</h1>")
+  });
+  /* Страница волейбольного мяча*/
+router.get('/volleyball', function(req, res, next) {
+  res.send("<h1>Страница волейбольного мяча</h1>")
+  });
+  
 module.exports = router;
