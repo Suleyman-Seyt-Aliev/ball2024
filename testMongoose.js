c93a9089cc07ba7964869f69d18aafb65b4152b8
unknown <suleyman15.05.2005@gmail.com>
2025-01-19 11:07:05 +0300

    ball_7_3

--- testMongoose.js
+++ testMongoose.js
@@ -1,11 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/testMongoose2024');
-var schema = mongoose.Schema({ name: String })
-
-schema.methods.shlyop = function(){
-console.log(this.name + " отбился от пола")
-}
-
-const Ball = mongoose.model('Ball', schema);
-const auto = new Ball({ name: 'Баскетбольный мяч' });
-auto.save().then(() => auto.shlyop());
+var Ball = require('./models/ball.js').Ball
+var ball = new Ball({
+title: "Баскетбольный мяч",
+nick: "basketball",
+})
+ball.save();
