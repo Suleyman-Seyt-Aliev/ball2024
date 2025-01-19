42186d365a54847f52b003c5962f44bea4fdb9cf
unknown <suleyman15.05.2005@gmail.com>
2025-01-19 11:31:25 +0300

    ball_8_3

--- routes/balls.js
+++ routes/balls.js
@@ -1,15 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var Ball = require('../models/ball').Ball;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('Новый маршрутизатор, для маршрутов, начинающихся с balls');
 });
 
-/* Страница мячиков */
-router.get("/:nick", function(req, res, next) {
-    res.send(req.params.nick);
+/* Страница машин */
+router.get("/:nick", async function(req, res, next) {
+    var balls = await Ball.find({nick: req.params.nick});
+    console.log(balls)
+    if(!balls.length) return next(new Error(" У нас нет информации об этом мяче! "))
+    var ball = balls[0];
+    res.render('ball', {
+    title: ball.title,
+    picture: ball.avatar,
+    desc: ball.desc
+    })
     });
-    
 
 module.exports = router;
