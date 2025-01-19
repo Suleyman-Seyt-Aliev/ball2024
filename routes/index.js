a8170067c47e23b9d9e7187ed89af13ac43edfcd
unknown <suleyman15.05.2005@gmail.com>
2025-01-19 22:29:24 +0300

    ball_10_6

--- routes/index.js
+++ routes/index.js
@@ -4,41 +4,43 @@ var User = require('../models/user').User;
 
 /* GET home page. */
 
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   req.session.greeting = "Hi!!!";
-  res.render('index', { title: 'Express',  counter:req.session.counter , picture: "images/welcome.jpg"});
+  res.render('index', { title: 'Express', counter: req.session.counter, picture: "images/welcome.jpg" });
 });
 
 /* GET login/registration page. */
-router.get('/logreg', function(req, res, next) {
-  res.render('logreg',{title: 'Вход'});
-  });
-  
-   /* POST login/registration page. */
-   router.post('/logreg', async function(req, res, next) {
-    var username = req.body.username
+router.get('/logreg', function (req, res, next) {
+  res.render('logreg', { title: 'Вход', error: null });
+});
+
+
+/* POST login/registration page. */
+router.post('/logreg', async function (req, res, next) {
+  var username = req.body.username
   var password = req.body.password
   console.log(username);
   console.log(password);
-  var users = await User.find({username: username});
-   console.log(users);
-   if (!users.length) {
+  var users = await User.find({ username: username });
+  console.log(users);
+  if (!users.length) {
     //res.send("<h1>Пользователь НЕ найден</h1>");
-    var user = new User({username:username,password:password})
-        await user.save();
-        req.session.user_id = user._id;
-        res.redirect('/');
-   } else 
-   {
+    var user = new User({ username: username, password: password })
+    await user.save();
+    req.session.user_id = user._id;
+    res.redirect('/');
+  } else {
     //res.send("<h1>Пользователь найден</h1>");
-      var foundUser = users[0];
-      if(foundUser.checkPassword(password)){
-        req.session.user_id = foundUser._id
-        res.redirect('/')
-      } else {
-        res.render('logreg',{title: 'Вход'});
-      }
-   }
+    var foundUser = users[0];
+    if (foundUser.checkPassword(password)) {
+      req.session.user_id = foundUser._id
+      res.redirect('/')
+    } else {
+      res.render('logreg', {
+        title: 'Вход', error: 'Пароль не верный'
+      });
+    }
+  }
 });
 
 module.exports = router;
