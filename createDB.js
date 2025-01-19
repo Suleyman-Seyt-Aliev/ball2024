2cead48b9b3de7f812a6e5df652c30ed3d2855c9
unknown <suleyman15.05.2005@gmail.com>
2025-01-19 10:23:45 +0300

    ball_6_3

--- createDB.js
+++ createDB.js
@@ -1,20 +1,20 @@
 const { MongoClient } = require('mongodb');
 // or as an es module:
 // import { MongoClient } from 'mongodb'
+var data = require("./data.js").data;
 // Connection URL
 const url = 'mongodb://localhost:27017';
 const client = new MongoClient(url);
 // Database Name
-const dbName = 'test2024';
+const dbName = 'ball2024';
 async function main() {
 // Use connect method to connect to the server
 await client.connect();
 console.log('Connected successfully to server');
 const db = client.db(dbName);
-const collection = db.collection('documents');
+const collection = db.collection('balls');
 // the following code examples can be pasted here...
-const insertResult = await collection.insertMany([{ a: 1 }, { a:
-2 }, { a: 3 }]);
+const insertResult = await collection.insertMany(data);
 console.log('Inserted documents =>', insertResult);
 return 'done.';
 }
