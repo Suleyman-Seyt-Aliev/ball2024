47f1bd29df4ccd105a82320499af2a071c0de7fc
unknown <suleyman15.05.2005@gmail.com>
2025-01-19 20:38:52 +0300

    ball_10_2

---
+++ checkUser.js
@@ -0,0 +1,9 @@
+var mongoose = require("mongoose")
+mongoose.connect("mongodb://localhost/ball2024")
+var User = require("./models/user.js").User
+var first_user = new User({
+    username: "Vasya",
+    password: "qwerty"
+})
+first_user.save();
+//checkUser.js
