76a040deaf72381cc7edae945d5ccac139eb21ae
unknown <suleyman15.05.2005@gmail.com>
2025-01-19 15:00:05 +0300

    ball_9_3

--- app.js
+++ app.js
@@ -26,14 +26,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+var MongoStore = require('connect-mongo');
 app.use(session({
   secret: "Balls",
   cookie:{maxAge:60*1000},
   proxy: true,
   resave: true,
-  saveUninitialized: true
+  saveUninitialized: true,
+  store: MongoStore.create({mongoUrl:
+    'mongodb://localhost/ball2024'})
   }))
-  
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/balls', balls);
